4e705267429c48cf62637df8930fe179a8090e2c
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 14:25:28 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', 'PagesController@index');
 Route::get('/about','PagesController@about');
 Route::get('/services','PagesController@services');
+
+Route::resource('posts', 'PostsController');
