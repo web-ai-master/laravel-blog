c178830c5f5ae040f235ebf709030e22e7e93f83
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 09:57:54 +0530

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,4 +9,12 @@ class PagesController extends Controller
     public function index(){
         return view ('pages.index');
     }
+
+     public function about(){
+        return view ('pages.about');
+    }
+
+     public function services(){
+        return view ('pages.services');
+    }
 }
