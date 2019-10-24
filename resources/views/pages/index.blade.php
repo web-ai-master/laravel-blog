5501024df74a96c48ad738774766bf29682d7b6c
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 10:16:28 +0530

    Extends from layouts

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layouts.app')
 
-        <title>{{config('app.name','Laravel Blog')}}</title>
-    </head>
-    <body>
-       <h1>Laravel</h1>
-    </body>
-</html>
+@section ('content')
+   <h1>Latavel</h1>
+   <p>This is main page</p>
+@endsection
