71a59512b8b132f9e9213716b7c0c80a8578d83e
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 11:36:34 +0530

    Update services.blade.php

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,6 +1,12 @@
 @extends ('layouts.app')
 
 @section ('content')
-   <h1>Services</h1>
-   <p>This is services page</p>
+   <h1>{{$title}}</h1>
+   @if(count($services) > 0)
+   <ul>
+     @foreach ($services as $service)
+        <li>{{$service}}</li>
+     @endforeach
+   </ul>
+   @endif
 @endsection
