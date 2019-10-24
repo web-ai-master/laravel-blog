1310331b5f903764e45d13a111bb8238a1d2b64b
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 09:57:20 +0530

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,12 @@
-hello
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About</title>
+    </head>
+    <body>
+       <h1>This is about page</h1>
+    </body>
+</html>
