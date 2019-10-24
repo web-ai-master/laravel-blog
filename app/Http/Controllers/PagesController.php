c7d98d850c6509e10dcd4a80fc5562219b955f6b
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 11:36:58 +0530

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,14 +7,20 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view ('pages.index');
+        $title = 'Welcome to Laravel';
+        return view ('pages.index', compact ('title'));
     }
 
      public function about(){
-        return view ('pages.about');
+        $title = 'About Us';
+        return view ('pages.about')->with ('title',$title);
     }
 
      public function services(){
-        return view ('pages.services');
+         $data = array(
+         'title' => 'Services',
+         'services' => ['Wed Design', 'SEO']
+         );
+        return view ('pages.services') ->with ($data);
     }
 }
