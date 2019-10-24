c7a2266175a52e8f08ea364ab3ea2d9f7d0395f7
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 19:39:33 +0530

    Update navbar.blade.php
    Add post create button

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -18,6 +18,9 @@
               <li><a href="/services">Services</a></li>
               <li><a href="/posts">Blog</a></li>
             </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/posts/create">Create Posts</a></li>
+            </ul>
         </div>
     </div>
 </nav>
