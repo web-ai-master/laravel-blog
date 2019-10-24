32d0592dfbd5c0fdc5acd319e232ed26f6b2f737
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 19:38:51 +0530

    Update create.blade.php
    Correct the error in the PostsController

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <h1>Create Post</h1>
-   {!! Form::open(['action'=>'PostController@store','method' => 'POST']) !!}
+   {!! Form::open(['action'=>'PostsController@store','method' => 'POST']) !!}
       <div class="form-group">
           {{Form::label('title','Title')}}
           {{Form:: text ('title','',['class' =>'form-control','placeholder'=>'Title'])}}
