009ea9dd177b3965dcb4cec585d4dd710083b6a1
jovan0901 <jovanvelickovic0901@gmail.com>
2019-10-24 12:04:22 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{asset('sass/app.scss')}}">
         <title>{{config('app.name','Laravel Blog')}}</title>
     </head>
     <body>
